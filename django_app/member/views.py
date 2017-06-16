318f30659c96e97a90ed6bc0f9e7ef4efa020a2c
Jung YunHo <saijdu2198@gmail.com>
2017-06-15 22:05:42 -0700

    use LoginForm

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout, get_user_model
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
+from .forms import LoginForm
 
 User = get_user_model()
+
 
 def login(request):
     # member/login.html 생성
@@ -17,23 +19,35 @@ def login(request):
     if request.method == 'POST':
         # 요청받은 POST데이터에서 username, password키가 가진 값들을
         # username, password변수에 할당 (문자열)
-        username = request.POST['username']
-        password = request.POST['password']
+        # username = request.POST['username']
+        # password = request.POST['password']
         # authenticate함수를 사용해서 User객체를 얻어 user에 할당
         # 인증에 실패할 경우 user변수에는 None이 할당됨
-        user = authenticate(
-            request,
-            username=username,
-            password=password,
-        )
-        # user변수가 None이 아닐 경우 (정상적으로 인증되어 User객체를 얻은 경우)
-        if user is not None:
-            # Django의 session을 이용해 이번 request와 user객체를 사용해 로그인 처리
-            # 이후의 request/response에서는 사용자가 인증된 상태로 통신이 이루어진다
+        # user = authenticate(
+        #     request,
+        #     username=username,
+        #     password=password,
+        # )
+        # if user is not None:
+        # # Django의 session을 이용해 이번 request와 user객체를 사용해 로그인 처리
+        #     # 이후의 request/response에서는 사용자가 인증된 상태로 통신이 이루어진다
+        #     django_login(request, user)
+        #     # 로그인 완료 후에는 post_list뷰로 리다이렉트 처리
+        #     return redirect('post:post_list')
+        # # user변수가 None일 경우 (username 또는 password가 틀려 인증에 실패한 경우)
+
+        # Form 클래스 사용시
+        # Bound form 생성
+        form = LoginForm(data=request.POST)
+        # Bound form의 유효성을 검증 통과하면 true bool 반환
+        if form.is_valid():
+            user = form.cleaned_data['user']
             django_login(request, user)
-            # 로그인 완료 후에는 post_list뷰로 리다이렉트 처리
             return redirect('post:post_list')
-        # user변수가 None일 경우 (username 또는 password가 틀려 인증에 실패한 경우)
+
+
+        # user변수가 None이 아닐 경우 (정상적으로 인증되어 User객체를 얻은 경우)
+        #
         else:
             # 로그인에 실패했음을 알림
             return HttpResponse('Login credentials invalid')
@@ -44,7 +58,11 @@ def login(request):
         # 아닐경우 login.html을 render해서 리턴
         if request.user.is_authenticated:
             return redirect('post:post_list')
-        return render(request, 'member/login.html')
+        form = LoginForm() # unbound form
+        context = {
+            'form':form,
+        }
+        return render(request, 'member/login.html', context=context)
 
 
         # render, redirect, httpresponse, ??
@@ -74,10 +92,10 @@ def signup(request):
     else:
         return render(request, 'member/signup.html')
 
-    # url은 /member/signup/$
-    # member/signup.html을 사용
-    #   username, password1, password2를 받아 회원가입
-    #   이미 유저가 존재하는지 검사
-    #   password1, 2가 일치하는지 검사
-    #   각각의 경우를 검사해서 틀릴 경우 오류메시지 리턴
-    #   가입에 성공시 로그인시키고 post_list로 리다이렉트
+        # url은 /member/signup/$
+        # member/signup.html을 사용
+        #   username, password1, password2를 받아 회원가입
+        #   이미 유저가 존재하는지 검사
+        #   password1, 2가 일치하는지 검사
+        #   각각의 경우를 검사해서 틀릴 경우 오류메시지 리턴
+        #   가입에 성공시 로그인시키고 post_list로 리다이렉트
